a9e7c1f36714e3dffe72658317f10232c7b6adf7
Ashwini Pagar <ashwini.pagar@techno-soft.in>
2021-04-13 20:01:05 +0530

    Profile screen bottom links done

--- app/Screens/ProfileScreen.js
+++ app/Screens/ProfileScreen.js
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, FlatList, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    Button, 
+    FlatList, 
+    ScrollView, 
+    TouchableOpacity, 
+    Image 
+} from 'react-native';
 
 const ProfileScreen = () => {
     const [data, setData] = useState([
@@ -45,37 +55,89 @@ const ProfileScreen = () => {
         },
     ])
     return (
-        <View style={styles.container}>
-            <Text>Profile Screen</Text>
+        <StyledTopView>
             <FlatList 
                 keyExtractor={(item) => item.id}
                 data={data}
                 renderItem={({ item }) => (
-                    <Button 
-                        title={item.name}
-                    />
+                    <TouchableOpacity>
+                        <StyledSpace></StyledSpace>
+                        <StyledText>{item.name}</StyledText>
+                        <StyledImage source={require('../../assets/images/arrow.png')} />
+                    </TouchableOpacity>
                 )}
             />
-            <ScrollView>
-                <Image source={require('../../assets/images/profile.png')} style={{width: 50, height: 50}} />
-                <Image source={require('../../assets/images/msg.png')} style={{width: 50, height: 50}} />
-                <Image source={require('../../assets/images/home.png')} style={{width: 50, height: 50}} />
-                <Image source={require('../../assets/images/money.png')} style={{width: 50, height: 50}} />
-                <Image source={require('../../assets/images/watch.png')} style={{width: 50, height: 50}} />
-            </ScrollView>
-        </View>
+            <StyledOuterView>
+                <StyledView>
+                    <StyledListImage source={require('../../assets/images/profile.png')} style={{width: 30, height: 30}} />
+                    <StyledListImage source={require('../../assets/images/msg.png')} style={{width: 30, height: 30}} />
+                    <StyledListImage source={require('../../assets/images/home.png')} style={{width: 30, height: 30}} />
+                    <StyledListImage source={require('../../assets/images/money.png')} style={{width: 30, height: 30}} />
+                    <StyledListImage source={require('../../assets/images/watch.png')} style={{width: 30, height: 30}} />
+                </StyledView>
+            </StyledOuterView>
+        </StyledTopView>
     )
 }
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignContent:'center'
-    },
-    icon: {
-        flex:1,
-    }
-})
+const StyledText = styled.Text`
+    color: #073259;
+    font-size: 20px;
+    background: #fff;
+    width: 90%;
+    padding: 15px 15px 15px 40px;
+    margin: 10px auto;
+    border-radius: 10px;
+    box-shadow: 10px 10px 8px #000;
+    position: relative;
+`;
+
+const StyledTopView = styled.View`
+    width: 100%;
+    height: 100%;
+    padding-top: 100px;
+    position: relative;
+`;
+
+const StyledSpace = styled.Text`
+    position: absolute;
+    top: 12px;
+    left: 20px;
+    width: 12px;
+    height: 55px;
+    z-index: 1;
+    background:  #073259;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+`;
+
+const StyledImage = styled.Image`
+    width: 12px;
+    height: 20px;
+    position: absolute;
+    top: 25px;
+    right: 35px;
+`;
+
+const StyledOuterView = styled.View`
+    background: #fff;
+    padding: 5px 0;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+`;
+
+const StyledView = styled.View`
+    width: 85%;
+    display: flex;
+    margin: 10px auto;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const StyledListImage = styled.Image`
+    padding: 0px;
+`;
