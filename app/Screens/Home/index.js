fd8c8863f164f3d3a5983db4e206ef6c19da374b
shubham.pol <shubham.pol@zeuslearning.com>
2021-05-04 16:40:56 +0530

    slider added in home screen

--- app/Screens/Home/index.js
+++ app/Screens/Home/index.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
-import { Image } from 'react-native';
-import { Container, Grid, Col, Row, Content, StyleProvider } from 'native-base';
+import { Image, View, Dimensions, StyleSheet } from 'react-native';
+import {
+    Container,
+    Grid,
+    Col,
+    Row,
+    Content,
+    StyleProvider,
+    Slider,
+    Box,
+    Root,
+    Card,
+} from 'native-base';
+
+import { SliderBox } from 'react-native-image-slider-box';
 // import { Picker } from '@react-native-picker/picker';
 // import { Picker } from '@react-native-community/picker';
 import getTheme from '../../utils/native-base-theme/components';
@@ -10,16 +23,63 @@ import HomeCard from './HomeCard';
 import FooterComponent from '../../Components/layout/FooterComponent';
 import HeaderComponent from '../../Components/layout/HeaderComponent';
 
+const WINDOW_WIDTH = Dimensions.get('window').width;
+
 const HomeScreen = ({ navigation }) => {
     // const [selectedLanguage, setSelectedLanguage] = useState();
 
     const TransportIcon = require('../../../assets/images/Transport.png');
     const CourierIcon = require('../../../assets/images/Courier.png');
+    const images = [
+        require('../../../assets/images/homeSlider1.png'),
+        'https://source.unsplash.com/1024x768/?water',
+        'https://source.unsplash.com/1024x768/?girl',
+        'https://source.unsplash.com/1024x768/?tree',
+    ];
     return (
         <StyleProvider style={getTheme(material)}>
             <Container>
                 <HeaderComponent />
                 <Content padder>
+                    <SliderBox
+                        images={images}
+                        sliderBoxHeight={200}
+                        onCurrentImagePressed={index =>
+                            console.log(`image ${index} pressed`)
+                        }
+                        dotColor="#FFEE58"
+                        inactiveDotColor="#90A4AE"
+                        paginationBoxVerticalPadding={20}
+                        autoplay
+                        circleLoop
+                        resizeMethod={'resize'}
+                        resizeMode={'cover'}
+                        paginationBoxStyle={{
+                            position: 'absolute',
+                            bottom: 0,
+                            padding: 0,
+                            alignItems: 'center',
+                            alignSelf: 'center',
+                            justifyContent: 'center',
+                            paddingVertical: 10,
+                            shadowColor: '#000',
+                            shadowOffset: { width: 0, height: 1 },
+                            shadowOpacity: 0.8,
+                            shadowRadius: 2,
+                            elevation: 5,
+                        }}
+                        dotStyle={{
+                            width: 0,
+                            height: 0,
+                        }}
+                        ImageComponentStyle={{
+                            borderRadius: 15,
+                            width: '97%',
+                            marginTop: 5,
+                        }}
+                        imageLoadingColor="#2196F3"
+                        parentWidth={WINDOW_WIDTH - 20}
+                    />
                     <Grid>
                         <Row>
                             <Col>
@@ -66,3 +126,11 @@ const HomeScreen = ({ navigation }) => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: '#000',
+        shadowOpacity: 1,
+        shadowRadius: 5,
+    },
+});
